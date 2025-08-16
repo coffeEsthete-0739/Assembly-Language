ce2bcfe83f8302df2fad5e15a9161c0c692e114d
Shakib Ahmed Numan <135098240+coffeEsthete-0739@users.noreply.github.com>
2025-08-16 22:17:12 +0600

    Average_of_N_numbers.asm

--- Average_of_N_numbers.asm
+++ Average_of_N_numbers.asm
@@ -1,13 +1,32 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
-    arr db 5, 10, 15, 20, 25
+    arr db 5 dup(?)    
     len equ 5
+    msg db "Enter a number: $"
 .CODE
 MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
 
+    MOV CX, len        
+    MOV SI, OFFSET arr  
+
+inputLoop:
+    ; show message
+    MOV AH, 9
+    LEA DX, msg
+    INT 21H
+
+    ; read one character
+    MOV AH, 1
+    INT 21H
+    SUB AL, 30H        
+    MOV [SI], AL       
+
+    INC SI             
+    LOOP inputLoop    
+    ; now calculate sum (same as before)
     MOV CX, len
     MOV SI, OFFSET arr
     XOR AX, AX
@@ -18,7 +37,7 @@ sumLoop:
     LOOP sumLoop
 
     MOV BL, len
-    DIV BL       ; AL/BL → quotient in AL
+    DIV BL             
 
     ADD AL, 30H
     MOV DL, AL
